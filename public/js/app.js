b3cc7af4f01bd9121e2825facbd5b41e7f9a131a
Moaz Ali <moazali626@gmail.com>
2021-01-03 18:36:08 +0500

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,18 +5,16 @@ const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
 formData.addEventListener("submit", (e) => {
-  fetch("http://localhost:3000/weather?address=" + search.value).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          messageTwo.textContent = "";
-          return;
-        }
-        messageOne.textContent = "Forcast: " + data.forcast;
-        messageTwo.textContent = "Location: " + data.location;
-      });
-    }
-  );
+  fetch("/weather?address=" + search.value).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
+        return;
+      }
+      messageOne.textContent = "Forcast: " + data.forcast;
+      messageTwo.textContent = "Location: " + data.location;
+    });
+  });
   e.preventDefault();
 });
--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const geo = require("./util/geo");
 const forcast = require("./util/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
@@ -67,6 +69,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Service is up and running successfully!");
+app.listen(port, () => {
+  console.log("Service is up on port " + port);
 });
